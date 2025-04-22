8ef5c905f853a4024e0085f959fb1235b5f6a579
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2025-04-22 07:33:28 -0300

    feat - Configurando path para interpretação das variáveis de cores criadas

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@theme': path.resolve(__dirname, 'src/theme'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@theme/colors.scss" as *;`,
+      },
+    },
+  },
 })
